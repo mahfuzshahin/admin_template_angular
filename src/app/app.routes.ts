c24d48619f7ca20e833d657109e43a223708e150
Mahfuz Shahin <mr.shahin94@gmail.com>
2025-10-08 10:09:49 +0600

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,16 @@ import {LoginComponent} from "./login/login.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {BasicFormComponent} from "./basic-form/basic-form.component";
 import {BasicTableComponent} from "./basic-table/basic-table.component";
+import {ModalUiComponent} from "./modal-ui/modal-ui.component";
+import {BasicUiElementComponent} from "./basic-ui-element/basic-ui-element.component";
+import {AuthGuard} from "./auth.guard";
 
 export const routes: Routes = [
-  { path: 'category', component: CategoryComponent },
+  { path: 'category', component: CategoryComponent, canActivate: [AuthGuard] },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'basic-form', component: BasicFormComponent },
   { path: 'basic-table', component: BasicTableComponent },
+  { path: 'modal-ui', component: ModalUiComponent },
+  { path: 'basic-ui-element', component: BasicUiElementComponent },
   { path: '', component: LoginComponent }
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+  constructor(private router: Router) {}
+  onLogin() {
+    // Example: hardcoded user for demo
+    const dummyUser = { email: 'admin@example.com', password: '123456' };
 
+    if (this.email === dummyUser.email && this.password === dummyUser.password) {
+      // Save user data in sessionStorage
+      sessionStorage.setItem('user', JSON.stringify(dummyUser));
+
+      // Redirect to dashboard
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
 }
